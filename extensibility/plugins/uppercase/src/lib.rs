63fb214fbef9b8eca1b3622bd0f98ee80f00f5bd
Thorsten Hans <thorsten.hans@fermyon.com>
2024-04-24 11:09:50 +0200

    fix: make uppercase plugin compile again
    Signed-off-by: Thorsten Hans <thorsten.hans@fermyon.com>

--- extensibility/plugins/uppercase/src/lib.rs
+++ extensibility/plugins/uppercase/src/lib.rs
@@ -2,10 +2,12 @@ mod bindings;
 
 use bindings::Guest;
 
-struct Component;
+pub struct Component;
 
 impl Guest for Component {
     fn transform(input: wit_bindgen::rt::string::String) -> wit_bindgen::rt::string::String {
         input.to_uppercase()
     }
 }
+
+bindings::export!(Component with_types_in bindings);
